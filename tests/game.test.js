82a56494fb64d83701eb48b5fe5fd194cd46d95f
AruakeMourao <54079601+AruakeMourao@users.noreply.github.com>
2023-10-07 10:39:58 -0300

    Create game.test.js

---
+++ tests/game.test.js
@@ -0,0 +1,16 @@
+// tests/game.test.js
+const TicTacToe = require('../src/game');
+
+describe('TicTacToe', () => {
+  it('deve criar um novo jogo vazio', () => {
+    const game = new TicTacToe();
+    expect(game.board).toEqual([
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ]);
+    expect(game.currentPlayer).toBe('X');
+    expect(game.winner).toBe(null);
+    expect(game.isDraw()).toBe(false);
+  });
+});
